5798b4065f5f451db56eb2624a0ef33894b89e6b
InterN <mz.anahita@gmail.com>
2020-03-15 12:57:19 +0330

    Update pptpd.sh
    change Y to y in line 5

--- pptpd.sh
+++ pptpd.sh
@@ -2,7 +2,7 @@ echo "Hi ! root "
 echo " You need root access to install "
 echo "please insert the valid info for create  vpn server "
 echo "Don't worry ..."
-echo "please insert (Y) key to Start... "
+echo "please insert (y) key to Start... "
 
 read ykey
 
