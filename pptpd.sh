0f510878b1ec02247ef63a1ae8e51853b476bdc8
InterN <mz.anahita@gmail.com>
2020-03-15 13:10:23 +0330

    Update pptpd.sh
    noting

--- pptpd.sh
+++ pptpd.sh
@@ -1,5 +1,5 @@
 echo "Hi ! root "
-echo " You need root access to install "
+echo "You need root access to install "
 echo "please insert the valid info for create  vpn server "
 echo "Don't worry ..."
 echo "please insert (y) key to Start... "
