a314ff08ed3e69b7db1970d7fe16c544ac994b62
InterN <mz.anahita@gmail.com>
2020-03-15 12:49:46 +0330

    Create pptpd.sh
    welcome message for user

---
+++ pptpd.sh
@@ -0,0 +1,6 @@
+echo "Hi ! root "
+echo " You need root access to install "
+echo "please insert the valid info for create  vpn server "
+echo "Don't worry ..."
+echo "please insert (Y) key to Start... "
+
