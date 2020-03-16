59bff2f39e4f82cc94d0d0e98f9e5cbb381a3026
mehdi <mz.anahita@gmail.com>
2020-03-16 09:09:48 +0330

    Update pptpd.sh
    get server ip addres and userip from user

--- pptpd.sh
+++ pptpd.sh
@@ -12,6 +12,11 @@ FILE=/etc/pptpd.conf
 if [[ -f "$FILE" ]] 
 then
 echo "PPTPD Service Is Exist" 
+echo ""
+echo "Please Insert Ip Server For Example : 192.114.16.112 "
+read ipserver
+echo "Please insert User Ip Range For Example : 192.114.16.113-120 "
+read userip
 else
 echo "pptpd service not found ... " 
 echo $(apt-get install pptpd -y )
